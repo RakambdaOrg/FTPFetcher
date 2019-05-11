42f606c3e39feb299953e44576d55c2493234516
Thomas Couchoud <zerderr@gmail.com>
2019-05-11 20:00:15 +0200

    Remove useless require

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,14 +5,15 @@
  * @since 2018-12-13
  */
 open module FTPFetcher {
-	requires java.scripting;
 	requires fr.mrcraftcod.utils.config;
-	requires org.slf4j;
+	
 	requires jsch;
 	requires jdeferred.core;
 	requires jcommander;
 	requires org.json;
 	requires org.apache.commons.io;
-	requires ch.qos.logback.classic;
 	requires progressbar;
+	
+	requires org.slf4j;
+	requires ch.qos.logback.classic;
 }
