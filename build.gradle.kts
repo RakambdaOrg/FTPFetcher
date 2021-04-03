16796fc0f754f04745d8525c7aadc9ec884816f2
Thomas Couchoud <thomas.couchoud@gmail.com>
2021-04-03 16:59:38 +0200

    Use gradle kotlin DSL and java 16

---
+++ build.gradle.kts
@@ -0,0 +1,102 @@
+plugins {
+    idea
+    java
+    application
+    id("com.github.johnrengelman.shadow") version ("6.1.0")
+    id("com.github.ben-manes.versions") version ("0.38.0")
+    id("io.freefair.lombok") version ("5.3.0")
+}
+
+group = "fr.raksrinana"
+description = "FTPFetcher"
+
+dependencies {
+    implementation(libs.jsch) {
+        exclude(module = "commons-io")
+        exclude(module = "commons-lang3")
+    }
+
+    implementation(libs.slf4j)
+    implementation(libs.logback) {
+        exclude(group = "edu.washington.cs.types.checker", module = "checker-framework")
+        exclude(group = "javax.activation", module = "activation")
+    }
+
+    implementation(libs.bundles.raksrinanaUtils)
+
+    implementation(libs.commonsIo)
+    implementation(libs.progressbar)
+
+    implementation(libs.picocli)
+    implementation(libs.bundles.jackson)
+
+    compileOnly(libs.jetbrainsAnnotations)
+}
+
+repositories {
+    val githubRepoUsername: String by project
+    val githubRepoPassword: String by project
+
+    maven {
+        url = uri("https://maven.pkg.github.com/RakSrinaNa/JavaUtils/")
+        credentials {
+            username = githubRepoUsername
+            password = githubRepoPassword
+        }
+    }
+    maven {
+        url = uri("https://projectlombok.org/edge-releases")
+    }
+    mavenCentral()
+    jcenter()
+}
+
+tasks {
+    processResources {
+        expand(project.properties)
+    }
+
+    compileJava {
+        val moduleName: String by project
+        inputs.property("moduleName", moduleName)
+
+        options.encoding = "UTF-8"
+        options.isDeprecation = true
+
+        doFirst {
+            val compilerArgs = options.compilerArgs
+            compilerArgs.add("--module-path")
+            compilerArgs.add(classpath.asPath)
+            classpath = files()
+        }
+    }
+
+    shadowJar {
+        archiveBaseName.set(project.name)
+        archiveClassifier.set("shaded")
+        archiveVersion.set("")
+    }
+
+    wrapper {
+        val wrapperVersion: String by project
+        gradleVersion = wrapperVersion
+    }
+}
+
+application {
+    val moduleName: String by project
+    val className: String by project
+
+    mainClassName = className
+    mainModule.set(moduleName)
+    mainClass.set(className)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
+}
+
+lombok {
+    version.set("edge-SNAPSHOT")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "ftpfetcher"
+
+enableFeaturePreview("VERSION_CATALOGS")
