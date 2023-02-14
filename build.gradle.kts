d35518126a45b7617b1cc0921ddb94b38220a358
Thomas Couchoud <thomas.couchoud@gmail.com>
2023-02-14 17:50:36 +0100

    Bump lombok and use it directly without plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     application
     alias(libs.plugins.shadow)
     alias(libs.plugins.names)
-    alias(libs.plugins.lombok)
     alias(libs.plugins.jib)
 }
 
@@ -29,6 +28,9 @@ dependencies {
     implementation(libs.bundles.jackson)
 
     compileOnly(libs.jetbrainsAnnotations)
+    compileOnly(libs.lombok)
+
+    annotationProcessor(libs.lombok)
 }
 
 repositories {
